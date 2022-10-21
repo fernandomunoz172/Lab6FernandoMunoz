917314559c6f68fd7a6a73d4b5672d10a80ef47c
fernandomunoz172 <munozf312@gmail.com>
2022-10-20 23:51:13 -0600

    Lab6

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+        static void StackNoParams()
+        {
+            int number =100;
+            double number1=1_2;
+            short shortnumber=4;
+            char vowel='a';
+            bool condition=true;
+        }
+        static void StackWithParams(int number, char vowel, bool condition)
+        {
+
+        }
+         static void StackWithHeap(int number, int number2, bool condition, int[] array)
+        {
+            int num=300;
+            int num2=200;
+            
+
+
+        }
+        void StackWithHeap(int num1, int num2, int[] num3) 
+{
+    num1 = 0;
+    num2 = 0;
+    num3 = new int[3] {0, 1, 2};
+}
+void StackOver(int num) 
+{
+
+}
+
+    }
+}
